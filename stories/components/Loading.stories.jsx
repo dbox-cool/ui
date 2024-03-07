2725faf9405c1afae3c997073b91951b5dbaf2f4
Gabriel Belisario <gabrielbelisariof@gmail.com>
2024-03-06 23:28:55 -0400

    loading component and stories

---
+++ stories/components/Loading.stories.jsx
@@ -0,0 +1,15 @@
+import { Loading } from '../../src/components/Loading';
+
+/** @type {import('@storybook/react').Meta} */
+export default {
+    title: 'DBOX/Loading',
+    component: Loading,
+    tags: ["autodocs"],
+    parameters:{
+        layout: "centered"
+    }
+};
+
+/** @type {import('@storybook/react').StoryObj} */
+export const Primary = {
+};
