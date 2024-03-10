d2c2e1e63b2a06c3ca38762d0908c8ace15b4f56
Gabriel Belisario <gabrielbelisariof@gmail.com>
2024-03-09 23:01:26 -0400

    shadcn straight import

--- stories/components/Navbar.stories.jsx
+++ stories/components/Navbar.stories.jsx
@@ -1,10 +1,10 @@
-import { withRouter } from 'storybook-addon-react-router-v6';
+// import { withRouter } from 'storybook-addon-react-router-v6';
 import Navbar from '../../src/components/Navbar/Navbar';
 
 /** @type {import('@storybook/react').Meta} */
 export default {
     title: 'DBOX/Navbar',
-    decorators: [withRouter],
+    // decorators: [withRouter],
     component: Navbar,
     // tags: ["autodocs"],
     args: {
