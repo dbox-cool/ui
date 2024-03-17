4bcb8174d7d171defb4beaae6bec40f2b65522ff
Gabriel Belisario <gabrielbelisariof@gmail.com>
2024-03-17 10:00:54 -0400

    storybook 8

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,15 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: ["../stories/**/*.stories.@(js|jsx|mjs|ts|tsx|mdx)", "../stories/**/*.docs.mdx"],
+  stories: [
+    "../stories/**/*.@(mdx|stories.@(js|jsx|mjs|ts|tsx))",
+    "../stories/**/*.docs.mdx"
+  ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
+    "@storybook/addon-mdx-gfm"
   ],
   framework: {
     name: "@storybook/react-vite",
